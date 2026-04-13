d5d76a4c9be277df089d1b19e5c0208311d7226d
DonTasko <dontasko.geral@gmail.com>
2026-04-13 04:02:38 +0100

    Create amazon.js

---
+++ amazon.js
@@ -0,0 +1,25 @@
+// netlify/functions/amazon.js
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+  try {
+    // Aqui vais buscar os dados da tua Google Sheets
+    // Exemplo com Google Sheets API:
+    const sheetUrl = 'https://script.google.com/macros/s/SEU_ID_DA_SHEET/exec';
+    const response = await axios.get(sheetUrl);
+    
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(response.data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Erro ao carregar produtos' })
+    };
+  }
+};
